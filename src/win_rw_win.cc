57f5ec85e51ce33c3c4ff548dd9c42777d82791d
Caylan Larson <caylan@semireg.com>
2022-02-15 13:49:21 -0600

    Revert namespace

--- src/win_rw_win.cc
+++ src/win_rw_win.cc
@@ -47,116 +47,116 @@ namespace{
 
     	return s.str();
     }
+}
 
-    MY_NODE_MODULE_CALLBACK(WritePath)
+MY_NODE_MODULE_CALLBACK(WritePath)
+{
+    MY_NODE_MODULE_HANDLESCOPE;
+    REQUIRE_ARGUMENTS(iArgs, 2);
+
+    REQUIRE_ARGUMENT_STRING(iArgs, 0, path);
+
+    std::string data;
+    v8::Local<v8::Value> arg1(iArgs[1]);
+    if (!getStringOrBufferFromV8Value(arg1, data))
     {
-        MY_NODE_MODULE_HANDLESCOPE;
-        REQUIRE_ARGUMENTS(iArgs, 2);
+        RETURN_EXCEPTION_STR("Argument 1 must be a string or Buffer");
+    }
 
-        REQUIRE_ARGUMENT_STRING(iArgs, 0, path);
+    HANDLE handle = CreateFile(*path, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
+    if (handle == INVALID_HANDLE_VALUE)
+    {
+        std::string error_str("error on write open: ");
+        error_str += getLastErrorCodeAndMessage();
+        printf("%s", error_str.c_str());
+        RETURN_EXCEPTION_STR(error_str.c_str());
+    }
 
-        std::string data;
-        v8::Local<v8::Value> arg1(iArgs[1]);
-        if (!getStringOrBufferFromV8Value(arg1, data))
+    COMMTIMEOUTS cto;
+    GetCommTimeouts(handle, &cto);
+    cto.WriteTotalTimeoutConstant = 1500;
+    cto.WriteTotalTimeoutMultiplier = 0;
+    SetCommTimeouts(handle, &cto);
+
+    DWORD dataSize = (DWORD)data.size();
+    DWORD batchSize = 4294967295;
+    DWORD totalWritten = 0;
+
+    while (totalWritten < dataSize)
+    {
+        DWORD thisSize = (dataSize - totalWritten);
+        if (thisSize > batchSize)
         {
-            RETURN_EXCEPTION_STR("Argument 1 must be a string or Buffer");
+            thisSize = batchSize; // Avoiding terrible C++ max syntax
         }
 
-        HANDLE handle = CreateFile(*path, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
-        if (handle == INVALID_HANDLE_VALUE)
+        DWORD written;
+        // printf("DEBUG: about to write:%lu\n", thisSize);
+        BOOL writeOK = WriteFile(handle, (LPVOID)(data.c_str() + totalWritten), thisSize, &written, NULL);
+        // printf("DEBUG: written:%lu\n", written);
+
+        if (!writeOK)
         {
-            std::string error_str("error on write open: ");
+            std::string error_str("error on write: ");
             error_str += getLastErrorCodeAndMessage();
+            CloseHandle(handle);
             printf("%s", error_str.c_str());
             RETURN_EXCEPTION_STR(error_str.c_str());
         }
 
-        COMMTIMEOUTS cto;
-        GetCommTimeouts(handle, &cto);
-        cto.WriteTotalTimeoutConstant = 1500;
-        cto.WriteTotalTimeoutMultiplier = 0;
-        SetCommTimeouts(handle, &cto);
-
-        DWORD dataSize = (DWORD)data.size();
-        DWORD batchSize = 4294967295;
-        DWORD totalWritten = 0;
-
-        while (totalWritten < dataSize)
-        {
-            DWORD thisSize = (dataSize - totalWritten);
-            if (thisSize > batchSize)
-            {
-                thisSize = batchSize; // Avoiding terrible C++ max syntax
-            }
-
-            DWORD written;
-            // printf("DEBUG: about to write:%lu\n", thisSize);
-            BOOL writeOK = WriteFile(handle, (LPVOID)(data.c_str() + totalWritten), thisSize, &written, NULL);
-            // printf("DEBUG: written:%lu\n", written);
-
-            if (!writeOK)
-            {
-                std::string error_str("error on write: ");
-                error_str += getLastErrorCodeAndMessage();
-                CloseHandle(handle);
-                printf("%s", error_str.c_str());
-                RETURN_EXCEPTION_STR(error_str.c_str());
-            }
-
-            totalWritten += written;
-        }
-
-        CloseHandle(handle);
+        totalWritten += written;
     }
 
-    MY_NODE_MODULE_CALLBACK(ReadPath)
+    CloseHandle(handle);
+}
+
+MY_NODE_MODULE_CALLBACK(ReadPath)
+{
+    MY_NODE_MODULE_HANDLESCOPE;
+    REQUIRE_ARGUMENTS(iArgs, 1);
+
+    REQUIRE_ARGUMENT_STRING(iArgs, 0, path);
+
+    HANDLE handle = CreateFile(*path, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_FLAG_OVERLAPPED, 0);
+    if (handle == INVALID_HANDLE_VALUE)
     {
-        MY_NODE_MODULE_HANDLESCOPE;
-        REQUIRE_ARGUMENTS(iArgs, 1);
-
-        REQUIRE_ARGUMENT_STRING(iArgs, 0, path);
-
-        HANDLE handle = CreateFile(*path, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_FLAG_OVERLAPPED, 0);
-        if (handle == INVALID_HANDLE_VALUE)
-        {
-            std::string error_str("error on read open: ");
-            error_str += getLastErrorCodeAndMessage();
-            printf("%s", error_str.c_str());
-            RETURN_EXCEPTION_STR(error_str.c_str());
-        }
-
-        DWORD nRead;
-        char readBuf[1000] = {0};
-
-        OVERLAPPED oRead = {0};
-        oRead.hEvent = CreateEvent(NULL, FALSE, FALSE, NULL);
-
-        // printf("DEBUG: about to read\n");
-        BOOL readOK = ReadFile(handle, readBuf, 1000, &nRead, &oRead);
-
-        DWORD dwWaitRes = WaitForSingleObject(oRead.hEvent, 50);
-        DWORD reason = dwWaitRes - WAIT_OBJECT_0;
-        BOOL didTimeout = reason == WAIT_TIMEOUT;
-
-        readOK = GetOverlappedResult(handle, &oRead, &nRead, false);
-
-        // printf("DEBUG: closing\n");
-
-        CancelIo(handle);
-        CloseHandle(handle);
-        if (nRead > 999)
-        {
-            RETURN_EXCEPTION_STR("FIXME: too much data");
-        }
-
-        // printf("DEBUG: read: %lu\n", nRead);
-
-        if (!readOK && didTimeout)
-        {
-            RETURN_EXCEPTION_STR("Timeout");
-        }
-
-        v8::Local<v8::Object> js_buffer = node::Buffer::Copy(isolate, (const char *)readBuf, nRead).ToLocalChecked();
-        MY_NODE_MODULE_RETURN_VALUE(js_buffer);
+        std::string error_str("error on read open: ");
+        error_str += getLastErrorCodeAndMessage();
+        printf("%s", error_str.c_str());
+        RETURN_EXCEPTION_STR(error_str.c_str());
     }
+
+    DWORD nRead;
+    char readBuf[1000] = {0};
+
+    OVERLAPPED oRead = {0};
+    oRead.hEvent = CreateEvent(NULL, FALSE, FALSE, NULL);
+
+    // printf("DEBUG: about to read\n");
+    BOOL readOK = ReadFile(handle, readBuf, 1000, &nRead, &oRead);
+
+    DWORD dwWaitRes = WaitForSingleObject(oRead.hEvent, 50);
+    DWORD reason = dwWaitRes - WAIT_OBJECT_0;
+    BOOL didTimeout = reason == WAIT_TIMEOUT;
+
+    readOK = GetOverlappedResult(handle, &oRead, &nRead, false);
+
+    // printf("DEBUG: closing\n");
+
+    CancelIo(handle);
+    CloseHandle(handle);
+    if (nRead > 999)
+    {
+        RETURN_EXCEPTION_STR("FIXME: too much data");
+    }
+
+    // printf("DEBUG: read: %lu\n", nRead);
+
+    if (!readOK && didTimeout)
+    {
+        RETURN_EXCEPTION_STR("Timeout");
+    }
+
+    v8::Local<v8::Object> js_buffer = node::Buffer::Copy(isolate, (const char *)readBuf, nRead).ToLocalChecked();
+    MY_NODE_MODULE_RETURN_VALUE(js_buffer);
 }
